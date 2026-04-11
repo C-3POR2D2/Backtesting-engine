1f70c605160c66e7d7ca84d058dd8f0fb1bf981d
Mandelorian <tewoflosfassil@gmail.com>
2026-04-11 19:11:42 -0400

    base class dedinitions

---
+++ models/events.py
@@ -0,0 +1,32 @@
+import time 
+
+class Event():
+    def __init__(self, timestamp):
+        self.timestamp = timestamp
+
+
+class MarketDataEvent(Event):
+    def __init__(self, timestamp, volume, open, high, low, close):
+        super().__init__(timestamp)
+        self.volume = volume 
+        self.open = open 
+        self.close = close
+        self.high = high 
+        self.low = low
+
+class OrderEvent(Event):
+    def __init__(self, timestamp, price, id, order_type, orderside):
+        super().__init__(timestamp)
+        self.id = id
+        self.price = price
+        self.ordertype = order_type
+        self.orderside = orderside
+    
+class FillEvent(Event):
+    def __init__(self, timestamp, fill_price, Ticker, num_of_shares, order_side):
+        super().__init__(timestamp)
+        self.Ticker = Ticker
+        self.fillprice = fill_price
+        self.numOfShares = num_of_shares
+        self.orderside= order_side
+        
