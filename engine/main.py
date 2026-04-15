547cae74c3a2c6bb7afb14f66a438053e1f6a820
Mandelorian <tewoflosfassil@gmail.com>
2026-04-15 10:15:03 -0400

    implemented:main.py based on previous logic

--- engine/main.py
+++ engine/main.py
@@ -3,30 +3,43 @@ from data.data_handler import DataHandler
 from models.events import MarketDataEvent, OrderEvent, FillEvent
 from strategy.strategy import Strategy
 from engine.execution_handler import ExecutionHandler
+from portfolio.portfolio_tracker import PortfolioTracker
 
-import importlib
-portfolios = importlib.import_module("portfolio tracker.portfolio_tracker")
-pt = portfolios.PortfolioTracker
 
-while DataHandler or EventQueue.is_empty()== False:
-    event = None
-    if EventQueue.is_empty == False:
 
-        event = EventQueue.get()
 
-        if event == MarketDataEvent: 
-            market_event = Strategy(event)
-        if event ==  OrderEvent:
-           market_event = ExecutionHandler(event)
-        if event == FillEvent(event)
+class Engine():
+    def __init__(self, data_handler, queue, strategy, execution_hander, portfolio_tracker):
+        self.data_handler = data_handler
+        self.queue = queue
+        self.strategy = strategy
+        self.execution_handler = execution_hander
+        self.portfolio_tracker = portfolio_tracker
+ 
 
-    else:
-        new_candle = DataHandler.send_market_data()
-# while data has candles OR queue is not empty:
-#     if queue is not empty:
-#         get event
-#         if MarketDataEvent → Strategy
-#         if OrderEvent → ExecutionHandler
-#         if FillEvent → PortfolioTracker
-#     else:
-#         ask DataHandler for next candle
+    def run(self):
+
+
+        while self.data_handler.check_data() or self.queue.is_empty() == False:
+            event = None
+            if self.queue.is_empty == False:
+
+                event = self.queue.get()
+
+                if isinstance(event, MarketDataEvent): 
+                    market_event = self.strategy.on_market_data_event(event)
+                if isinstance(event, OrderEvent):
+                    market_event = self.execution_handler.fill(event)
+                if isinstance(event, FillEvent):
+                    market_event = self.portfolio_tracker.on_fill_event(event)
+            else:
+                new_candle = self.data_handler.send_market_data()
+
+        # while data has candles OR queue is not empty:
+        #     if queue is not empty:
+        #         get event
+        #         if MarketDataEvent → Strategy
+        #         if OrderEvent → ExecutionHandler
+        #         if FillEvent → PortfolioTracker
+        #     else:
+        #         ask DataHandler for next candle
